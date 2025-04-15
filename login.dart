b08abd292a8b7caf1bec563c9b614e397d3802da
YudizAndroidKishan <kishan.sindhi@yudiz.com>
2025-04-15 15:58:39 +0530

    Auto-generated test cases for PR #1 Hello World

--- login.dart
+++ login.dart
@@ -1,6 +1,6 @@
 class Login extends StatelessWidget with WidgetsBindingObserver {
                 @override
                 Widget build(BuildContext context) {
-                    return Container();
+                    return SizedBox();
                 }
             }
